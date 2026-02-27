7d2522a0a88b77a5ef2fce81f7acf484ebfb8079
Martin Ivanov <marto1980@gmail.com>
2026-02-27 18:46:59 +0200

    Write Multiplication Table Java Program with LazyVim

---
+++ FirstJavaProject/src/main/java/com/marto/firstjavaproject/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package com.marto.firstjavaproject;
+
+/**
+ * @author marto
+ */
+public class MultiplicationTable {
+  public void print() {
+    for (int i = 1; i <= 10; i++) {
+      System.out.printf("%d * %d = %d", 5, i, 5 * i).println();
+    }
+  }
+}
---
+++ FirstJavaProject/src/main/java/com/marto/firstjavaproject/MultiplicationTableRunner.java
@@ -0,0 +1,13 @@
+package com.marto.firstjavaproject;
+
+/**
+ * @author marto
+ */
+public class MultiplicationTableRunner {
+
+  public static void main(String[] args) {
+    MultiplicationTable table = new MultiplicationTable();
+    table.print();
+
+  }
+}
