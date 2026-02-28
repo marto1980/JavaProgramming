a17e696cc7a877cc9f0565c99a86f4f54eca94cb
Martin Ivanov <marto1980@gmail.com>
2026-02-28 14:52:32 +0200

    Add Mode Methods for Multiplication

--- FirstJavaProject/src/main/java/com/marto/firstjavaproject/MultiplicationTable.java
+++ FirstJavaProject/src/main/java/com/marto/firstjavaproject/MultiplicationTable.java
@@ -3,10 +3,22 @@ package com.marto.firstjavaproject;
 /**
  * @author marto
  */
-public class MultiplicationTable {
-  public void print() {
+class MultiplicationTable {
+  void print() {
     for (int i = 1; i <= 10; i++) {
       System.out.printf("%d * %d = %d", 5, i, 5 * i).println();
+    }
+  }
+
+  void print(int table) {
+    for (int i = 1; i <= 10; i++) {
+      System.out.printf("%d * %d = %d", table, i, table * i).println();
+    }
+  }
+
+  void print(int table, int from, int to) {
+    for (int i = from; i <= to; i++) {
+      System.out.printf("%d * %d = %d", table, i, table * i).println();
     }
   }
 }
--- FirstJavaProject/src/main/java/com/marto/firstjavaproject/MultiplicationTableRunner.java
+++ FirstJavaProject/src/main/java/com/marto/firstjavaproject/MultiplicationTableRunner.java
@@ -7,7 +7,6 @@ public class MultiplicationTableRunner {
 
   public static void main(String[] args) {
     MultiplicationTable table = new MultiplicationTable();
-    table.print();
-
+    table.print(6, 2, 15);
   }
 }
