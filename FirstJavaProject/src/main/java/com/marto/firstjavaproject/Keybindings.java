7a5e8160ed8fe5f633e925d95a35ad8a60eb2bb4
Martin Ivanov <marto1980@gmail.com>
2026-02-28 18:50:35 +0200

    LazyVim vs JShell: How to Choose?

---
+++ FirstJavaProject/src/main/java/com/marto/firstjavaproject/Keybindings.java
@@ -0,0 +1,12 @@
+package com.marto.firstjavaproject;
+
+/**
+ * @author marto
+ */
+public class Keybindings {
+
+  public static void main(String[] args) {
+    int i = 0;
+    System.out.println(i);
+  }
+}
