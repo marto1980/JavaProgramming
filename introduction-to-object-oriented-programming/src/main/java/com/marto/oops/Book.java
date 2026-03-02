4e1fdde3feb6e170b33659a22494bab5736ab518
Martin Ivanov <marto1980@gmail.com>
2026-03-02 11:23:51 +0200

    Understand the Basics of Encapsulation

---
+++ introduction-to-object-oriented-programming/src/main/java/com/marto/oops/Book.java
@@ -0,0 +1,12 @@
+package com.marto.oops;
+
+/**
+ * @author marto
+ */
+public class Book {
+  private int noOfCopies;
+
+  void setNoOfCopies(int noOfCopies) {
+    this.noOfCopies = noOfCopies;
+  }
+}
