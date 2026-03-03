04a706102fc78739f5002eb17e31274bb15c7ec3
Martin Ivanov <marto1980@gmail.com>
2026-03-03 11:36:06 +0200

    Introduction to Nested If Else

--- conditionals-if-and-switch/src/main/java/com/marto/ifstatement/examples/IfStatementRunner.java
+++ conditionals-if-and-switch/src/main/java/com/marto/ifstatement/examples/IfStatementRunner.java
@@ -2,6 +2,13 @@ package com.marto.ifstatement.examples;
 
 public class IfStatementRunner {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    int i = 26;
+    if (i == 25) {
+      System.out.println("i is 25");
+    } else if (i == 24) {
+      System.out.println("i is 25");
+    } else {
+      System.out.println("i is neither 25 nor 24");
+    }
   }
 }
