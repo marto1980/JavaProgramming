b6664b7c47ac1e1a061873d12f3aafbd178a349e
Martin Ivanov <marto1980@gmail.com>
2026-03-03 11:46:39 +0200

    If Else Statement: Puzzle2

--- conditionals-if-and-switch/src/main/java/com/marto/ifstatement/examples/IfStatementRunner.java
+++ conditionals-if-and-switch/src/main/java/com/marto/ifstatement/examples/IfStatementRunner.java
@@ -2,7 +2,8 @@ package com.marto.ifstatement.examples;
 
 public class IfStatementRunner {
   public static void main(String[] args) {
-    puzzle1();
+    // puzzle1();
+    puzzle2();
   }
 
   public static void puzzle1() {
@@ -15,6 +16,19 @@ public class IfStatementRunner {
       System.out.println(3);
     } else {
       System.out.println(4);
+    }
+  }
+
+  private static void puzzle2() {
+    int l = 15;
+    if (l < 20) {
+      System.out.println("l < 20");
+    }
+
+    if (l > 20) {
+      System.out.println("l > 20");
+    } else {
+      System.out.println("Who am I?");
     }
   }
 }
