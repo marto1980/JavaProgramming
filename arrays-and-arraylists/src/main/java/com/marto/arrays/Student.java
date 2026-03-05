c3f8a6d5485134fa6aedb4bfc95461a4707e0628
Martin Ivanov <marto1980@gmail.com>
2026-03-05 19:00:34 +0200

    Introduce Variable Arguments

--- arrays-and-arraylists/src/main/java/com/marto/arrays/Student.java
+++ arrays-and-arraylists/src/main/java/com/marto/arrays/Student.java
@@ -11,7 +11,7 @@ public class Student {
   String name;
   int[] marks;
 
-  public Student(String name, int[] marks) {
+  public Student(String name, int... marks) {
     this.name = name;
     this.marks = marks;
   }
--- arrays-and-arraylists/src/main/java/com/marto/arrays/StudentRunner.java
+++ arrays-and-arraylists/src/main/java/com/marto/arrays/StudentRunner.java
@@ -4,8 +4,7 @@ import java.math.BigDecimal;
 
 public class StudentRunner {
   public static void main(String[] args) {
-    int[] marks = {97, 98, 100};
-    Student student = new Student("Ranga", marks);
+    Student student = new Student("Ranga", 97, 98, 100);
 
     int number = student.getNumberOfMarks();
     System.out.println("Number of marks: " + number);
