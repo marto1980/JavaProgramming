8a9b22dbebde0dc2c91a93d151e43fc443b4bf84
Martin Ivanov <marto1980@gmail.com>
2026-03-06 17:49:26 +0200

    Use Diamond Operator

--- object-oriented-programming-2/src/main/java/com/marto/oops/level2/Book.java
+++ object-oriented-programming-2/src/main/java/com/marto/oops/level2/Book.java
@@ -7,7 +7,7 @@ public class Book {
   private int id;
   private String title;
   private String author;
-  private List<Review> reviews = new ArrayList<Review>();
+  private List<Review> reviews = new ArrayList<>();
 
   public Book(int id, String title, String author) {
     this.id = id;
