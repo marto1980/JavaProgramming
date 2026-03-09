e0910cd52f6808ace9eeee0ff5af467f9ddc97d0
Martin Ivanov <marto1980@gmail.com>
2026-03-09 14:52:57 +0200

    Interface Puzzles

--- object-oriented-programming-2/src/main/java/com/marto/oops/level2/interfaces/Project.java
+++ object-oriented-programming-2/src/main/java/com/marto/oops/level2/interfaces/Project.java
@@ -5,6 +5,31 @@ package com.marto.oops.level2.interfaces;
  */
 public class Project {
 
+  interface Test {
+    void nothing();
+
+    default void nothing1() {}
+    ;
+  }
+
+  class Class1 implements Test {
+
+    @Override
+    public void nothing() {
+      // TODO Auto-generated method stub
+
+    }
+  }
+
+  class Class2 implements Test {
+
+    @Override
+    public void nothing() {
+      // TODO Auto-generated method stub
+
+    }
+  }
+
   public static void main(String[] args) {
     // ComplexAlgorithm algorithm = new DummyAlgorithm();
     ComplexAlgorithm algorithm = new RealAlgorithm();
