883206fe8794259450b4806946e65612c1bb3a75
Martin Ivanov <marto1980@gmail.com>
2026-03-10 09:10:26 +0200

    Introduce Polymorphism

--- object-oriented-programming-2/src/main/java/com/marto/oops/level2/interfaces/GameRunner.java
+++ object-oriented-programming-2/src/main/java/com/marto/oops/level2/interfaces/GameRunner.java
@@ -6,11 +6,18 @@ package com.marto.oops.level2.interfaces;
 public class GameRunner {
 
   public static void main(String[] args) {
+    GamingConsole[] games = {new MarioGame(), new ChessGame()};
+    for (GamingConsole game : games) {
+      game.up();
+      game.down();
+      game.left();
+      game.right();
+    }
     // GamingConsole game = new MarioGame();
-    ChessGame game = new ChessGame();
-    game.up();
-    game.down();
-    game.left();
-    game.right();
+    // ChessGame game = new ChessGame();
+    // game.up();
+    // game.down();
+    // game.left();
+    // game.right();
   }
 }
