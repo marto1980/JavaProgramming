0b32f63bce561cd8cf85b1a89137bb6608630529
Martin Ivanov <marto1980@gmail.com>
2026-03-12 11:14:12 +0200

    Implement generics for a Custom List

---
+++ generics/src/main/java/com/marto/generics/MyCustomList.java
@@ -0,0 +1,24 @@
+package com.marto.generics;
+
+import java.util.ArrayList;
+
+public class MyCustomList<T> {
+  private ArrayList<T> list = new ArrayList<>();
+
+  public void addElement(T element) {
+    list.add(element);
+  }
+
+  public void removeElement(T element) {
+    list.remove(element);
+  }
+
+  @Override
+  public String toString() {
+    return list.toString();
+  }
+
+  public T get(int index) {
+    return list.get(index);
+  }
+}
