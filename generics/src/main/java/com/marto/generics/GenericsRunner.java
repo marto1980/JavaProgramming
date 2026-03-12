74293c7f539712999ee3ad12023b6ae1ac0a7acb
Martin Ivanov <marto1980@gmail.com>
2026-03-12 12:04:38 +0200

    Generics and WildCards: Upper Bound and Lower Bound

--- generics/src/main/java/com/marto/generics/GenericsRunner.java
+++ generics/src/main/java/com/marto/generics/GenericsRunner.java
@@ -8,11 +8,34 @@ public class GenericsRunner {
     return value;
   }
 
-  static <X extends List<T>, T> void duplicate(X list) {
+  static <X extends List<T>, T extends Number> void duplicate(X list) {
     list.addAll(list);
   }
 
+  static Number sumOfNumberList(List<? extends Number> numbers) {
+    double sum = 0.0;
+    for (Number number : numbers) {
+      sum += number.doubleValue();
+    }
+    return sum;
+  }
+
+  static void addACoupleOfValues(List<? super Number> numbers) {
+    numbers.add(1);
+    numbers.add(1.0);
+    numbers.add(1.0f);
+    numbers.add(1l);
+  }
+
   public static void main(String[] args) {
+    List<Number> emptyList = new ArrayList<>();
+    addACoupleOfValues(emptyList);
+    System.out.println(emptyList);
+
+    System.out.println(sumOfNumberList(List.of(1, 2, 3, 4, 5)));
+    System.out.println(sumOfNumberList(List.of(1.1, 2.1, 3.1, 4.1, 5.1)));
+    System.out.println(sumOfNumberList(List.of(1l, 2l, 3l, 4l, 5l)));
+
     String value1 = doubleValue(new String());
     Integer number1 = doubleValue(Integer.valueOf(5));
     List<Integer> list1 = doubleValue(new ArrayList<>());
