1b16975a80db76ae4ac396698efb322febc53ea9
Martin Ivanov <marto1980@gmail.com>
2026-03-12 14:05:19 +0200

    First Example of Functional Programming

--- functional-programming/src/main/java/com/marto/functionalprogramming/FunctionalProgrammingRunner.java
+++ functional-programming/src/main/java/com/marto/functionalprogramming/FunctionalProgrammingRunner.java
@@ -1,5 +1,22 @@
 package com.marto.functionalprogramming;
 
+import java.util.List;
+
 public class FunctionalProgrammingRunner {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    List<String> list = List.of("Apple", "Banana", "Cat", "Dog");
+
+    // printBasic(list);
+    printWithFP(list);
+  }
+
+  private static void printBasic(List<String> list) {
+    for (String string : list) {
+      System.out.println(string);
+    }
+  }
+
+  private static void printWithFP(List<String> list) {
+    list.stream().forEach(element -> System.out.println("element - " + element));
+  }
 }
