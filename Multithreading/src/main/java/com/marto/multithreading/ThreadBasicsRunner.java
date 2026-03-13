42de049fdacea5d26cc58a8e8597e666a5b5b17b
Martin Ivanov <marto1980@gmail.com>
2026-03-13 18:20:27 +0200

    Communication between Threads - join Method

--- Multithreading/src/main/java/com/marto/multithreading/ThreadBasicsRunner.java
+++ Multithreading/src/main/java/com/marto/multithreading/ThreadBasicsRunner.java
@@ -4,17 +4,23 @@ package com.marto.multithreading;
 // implements Runnable
 
 public class ThreadBasicsRunner {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     // Task 1
     System.out.println("Task 1 Kicked Off");
     Task1 task1 = new Task1();
+    task1.setPriority(1);
     task1.start();
 
     // Task 2
     System.out.println("Task 2 Kicked Off");
     Task2 task2 = new Task2();
     Thread task2Thread = new Thread(task2);
+    task2Thread.setPriority(10);
     task2Thread.start();
+
+    // wait for task 1 to complete
+    task1.join();
+    task2Thread.join();
 
     // Task 3
     System.out.println("Task 3 Kicked Off");
