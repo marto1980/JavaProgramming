bec97bd72ade7d5f68325c871b8439c219a435c6
Martin Ivanov <marto1980@gmail.com>
2026-03-13 15:19:37 +0200

    Functions are First-Class Citizens

--- functional-programming/src/main/java/com/marto/functionalprogramming/MethodReferencesRunner.java
+++ functional-programming/src/main/java/com/marto/functionalprogramming/MethodReferencesRunner.java
@@ -1,6 +1,7 @@
 package com.marto.functionalprogramming;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MethodReferencesRunner {
   public static void print(Integer number) {
@@ -9,6 +10,10 @@ public class MethodReferencesRunner {
 
   public static boolean isEven(Integer n) {
     return n % 2 == 0;
+  }
+
+  private static Predicate<? super Integer> createOddPredicate() {
+    return n -> n % 2 == 1;
   }
 
   public static void main(String[] args) {
@@ -20,11 +25,9 @@ public class MethodReferencesRunner {
         // .forEach(System.out::println);
         .forEach(MethodReferencesRunner::print);
 
-    Integer max =
-        List.of(23, 45, 67, 34).stream()
-            .filter(MethodReferencesRunner::isEven)
-            .max(Integer::compare)
-            .orElse(0);
+    Predicate<? super Integer> isEven = n -> n % 2 == 0;
+    Predicate<? super Integer> isOdd = createOddPredicate();
+    Integer max = List.of(23, 45, 67, 34).stream().filter(isEven).max(Integer::compare).orElse(0);
 
     System.out.println(max);
   }
