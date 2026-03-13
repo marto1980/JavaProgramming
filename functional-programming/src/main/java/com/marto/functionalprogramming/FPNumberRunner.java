25cb744ecdb3f6105e9781fac2213cf442a9925f
Martin Ivanov <marto1980@gmail.com>
2026-03-13 13:26:28 +0200

    Stream Intermediate Operations

--- functional-programming/src/main/java/com/marto/functionalprogramming/FPNumberRunner.java
+++ functional-programming/src/main/java/com/marto/functionalprogramming/FPNumberRunner.java
@@ -1,17 +1,26 @@
 package com.marto.functionalprogramming;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class FPNumberRunner {
 
   public static void main(String[] args) {
     List<Integer> numbers = List.of(4, 6, 8, 13, 3, 15);
     int sum = numbers.stream().reduce(0, (number1, number2) -> number1 + number2);
-    System.out.println("Sum of all: " + sum);
+    // System.out.println("Sum of all: " + sum);
 
     int sumOfOdds =
         numbers.stream()
             .reduce(0, (number1, number2) -> number1 + (number2 % 2 == 1 ? number2 : 0));
-    System.out.println("Sum of odds: " + sumOfOdds);
+    // System.out.println("Sum of odds: " + sumOfOdds);
+
+    IntStream.range(1, 11).map(e -> e * e).forEach(e -> System.out.println(e));
+
+    List<String> list = List.of("Apple", "Ant", "Bat");
+
+    list.stream().map(e -> e.toLowerCase()).forEach(e -> System.out.println(e));
+
+    list.stream().map(e -> e.length()).forEach(e -> System.out.println(e));
   }
 }
