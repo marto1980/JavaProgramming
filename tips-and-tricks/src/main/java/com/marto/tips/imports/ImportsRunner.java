cfcba8d4b19d7e946746fa6aed839fdb3ba4b66a
Martin Ivanov <marto1980@gmail.com>
2026-03-17 17:30:55 +0200

    Import Static Methods and Fields of Classes

---
+++ tips-and-tricks/src/main/java/com/marto/tips/imports/ImportsRunner.java
@@ -0,0 +1,18 @@
+package com.marto.tips.imports;
+
+import static java.lang.System.out;
+import static java.util.Collections.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class ImportsRunner {
+  public static void main(String[] args) {
+    // String str = "";
+    BigDecimal bd = null;
+    out.println("Imports");
+    out.println("Static Imports");
+
+    sort(new ArrayList<>());
+  }
+}
