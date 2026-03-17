67f933f34f82ae55edee71f5b540cf22a54cc2fc
Martin Ivanov <marto1980@gmail.com>
2026-03-17 10:52:43 +0200

    Recursively List and Filter All Files and Folders in a Directory

--- files/src/main/java/com/marto/files/DirectoryScanRunner.java
+++ files/src/main/java/com/marto/files/DirectoryScanRunner.java
@@ -2,10 +2,24 @@ package com.marto.files;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 public class DirectoryScanRunner {
   public static void main(String[] args) throws IOException {
-    Files.list(Paths.get(".")).forEach(System.out::println);
+    Path currentDirectory = Paths.get(".");
+    // Files.list(currentDirectory).forEach(System.out::println);
+    Predicate<? super Path> predicate = path -> String.valueOf(path).endsWith(".java");
+    // Files.walk(currentDirectory, 8).filter(predicate).forEach(System.out::println);
+
+    BiPredicate<Path, BasicFileAttributes> javaMatcher =
+        (path, attributes) -> String.valueOf(path).endsWith(".java");
+    BiPredicate<Path, BasicFileAttributes> directoryMatcher =
+        (path, attributes) -> attributes.isDirectory();
+    // Files.find(currentDirectory, 8, javaMatcher).forEach(System.out::println);
+    Files.find(currentDirectory, 8, directoryMatcher).forEach(System.out::println);
   }
 }
