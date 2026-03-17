05fd6740c900bf4de905e6eb5a61a317d8478899
Martin Ivanov <marto1980@gmail.com>
2026-03-17 18:40:28 +0200

    Class Access Modifiers: public and default

---
+++ tips-and-tricks/src/main/java/com/marto/tips/access/package1/ClassAccessModifiers.java
@@ -0,0 +1,7 @@
+package com.marto.tips.access.package1;
+
+// public, default
+public class ClassAccessModifiers {
+
+  public static void main(String[] args) {}
+}
---
+++ tips-and-tricks/src/main/java/com/marto/tips/access/package2/ClassAccessModifiersRunnerInOtherPackage.java
@@ -0,0 +1,10 @@
+package com.marto.tips.access.package2;
+
+import com.marto.tips.access.package1.ClassAccessModifiers;
+
+public class ClassAccessModifiersRunnerInOtherPackage {
+
+  public static void main(String[] args) {
+    ClassAccessModifiers c = new ClassAccessModifiers();
+  }
+}
