4cf8870fd0e42d3f712c36d46799c91a33c5297c
Martin Ivanov <marto1980@gmail.com>
2026-03-18 11:56:25 +0200

    Anonymous Classes

---
+++ tips-and-tricks/src/main/java/com/marto/tips/anonymous/AnonymousClassRunner.java
@@ -0,0 +1,32 @@
+package com.marto.tips.anonymous;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+// class LengthComparator implements Comparator<String> {
+//
+//   @Override
+//   public int compare(String str1, String str2) {
+//     return Integer.compare(str1.length(), str2.length());
+//   }
+// }
+
+public class AnonymousClassRunner {
+
+  public static void main(String[] args) {
+
+    List<String> animals = new ArrayList<>(List.of("Ant", "Cat", "Ball", "Elephant"));
+    Comparator<String> lengthComparator =
+        new Comparator<String>() {
+
+          @Override
+          public int compare(String str1, String str2) {
+            return Integer.compare(str1.length(), str2.length());
+          }
+        };
+    Collections.sort(animals, lengthComparator);
+    System.out.println(animals);
+  }
+}
