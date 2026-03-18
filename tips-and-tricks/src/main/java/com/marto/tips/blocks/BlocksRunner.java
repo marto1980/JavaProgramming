13d747e430e30e78a2922342096b3c3cb32cb35b
Martin Ivanov <marto1980@gmail.com>
2026-03-18 11:24:03 +0200

    Create Constants as static final

--- tips-and-tricks/src/main/java/com/marto/tips/blocks/BlocksRunner.java
+++ tips-and-tricks/src/main/java/com/marto/tips/blocks/BlocksRunner.java
@@ -1,10 +1,15 @@
 package com.marto.tips.blocks;
 
 public class BlocksRunner {
+  public static final int SECONDS_IN_MINUTE = 60;
+  public static final int MINITES_IN_HOUR = 60;
+  public static final int HOURS_IN_DAY = 24;
 
   public static void main(String[] args) {
+    System.out.println(Integer.MIN_VALUE);
+    System.out.println(SECONDS_IN_MINUTE * MINITES_IN_HOUR * HOURS_IN_DAY);
     if (3 > 2) {
-      System.out.println("3 > 2");
+      // System.out.println("3 > 2");
     }
   }
 }
