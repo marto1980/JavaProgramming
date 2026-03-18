f0144371be76aba612be054c8e7280a75fbb9521
Martin Ivanov <marto1980@gmail.com>
2026-03-18 10:22:07 +0200

    final Variables and final Arguments

--- tips-and-tricks/src/main/java/com/marto/tips/access/package1/FinalClass.java
+++ tips-and-tricks/src/main/java/com/marto/tips/access/package1/FinalClass.java
@@ -6,6 +6,10 @@ final class FinalClass {}
 
 class SomeClass {
   public final void doSomething() {}
+
+  public void doSomethingElse(final int arg) {
+    // arg = 6;
+  }
 }
 
 class ExtendingClass extends SomeClass {
--- tips-and-tricks/src/main/java/com/marto/tips/access/package1/FinalNonAccessModifierRunner.java
+++ tips-and-tricks/src/main/java/com/marto/tips/access/package1/FinalNonAccessModifierRunner.java
@@ -2,5 +2,9 @@ package com.marto.tips.access.package1;
 
 public class FinalNonAccessModifierRunner {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    final int i;
+    i = 7;
+    // i = 7;
+  }
 }
