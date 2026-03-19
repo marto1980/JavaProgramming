ab7f63566cbd790fb987fb90d43a83fac53b13fb
Martin Ivanov <marto1980@gmail.com>
2026-03-19 17:43:09 +0200

    Explore records

---
+++ java-new-api-features/src/main/java/com/marto/api/h/RecordsRunner.java
@@ -0,0 +1,26 @@
+package com.marto.api.h;
+
+public class RecordsRunner {
+
+  record Person(String name, String email, String phoneNumber) {
+    Person(String name, String email, String phoneNumber) {
+      if (name == null) {
+        throw new IllegalArgumentException("name is null");
+      }
+      this.name = name;
+      this.email = email;
+      this.phoneNumber = phoneNumber;
+    }
+  }
+
+  public static void main(String[] args) {
+    Person person = new Person("Ranga", "ranga@in28minutes.com", "123-456-7890");
+    Person person1 = new Person("Ranga", "ranga@in28minutes.com", "123-456-7890");
+    Person person2 = new Person("Ranga1", "ranga@in28minutes.com", "123-456-7890");
+
+    System.out.println(person);
+    System.out.println(person.equals(person1));
+    System.out.println(person.equals(person2));
+    System.out.println(person.name());
+  }
+}
