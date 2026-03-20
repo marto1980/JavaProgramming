93df956e5ab82f31f5b142a39d776c2700a205ff
Martin Ivanov <marto1980@gmail.com>
2026-03-20 16:51:57 +0200

    Deconstruction of records

--- java-new-api-features/src/main/java/com/marto/api/l/PatternMatchingRunner.java
+++ java-new-api-features/src/main/java/com/marto/api/l/PatternMatchingRunner.java
@@ -2,9 +2,29 @@ package com.marto.api.l;
 
 public class PatternMatchingRunner {
 
+  record Transaction(String sender, String receiver, int amount) {}
+
+  private static void printTransactionOld(Object objTransaction) {
+    if (objTransaction instanceof Transaction transaction) {
+      String sender = transaction.sender();
+      String receiver = transaction.receiver();
+      int amount = transaction.amount();
+
+      System.out.println(sender + " " + receiver + " " + amount);
+    }
+  }
+
+  private static void printTransactionNew(Object objTransaction) {
+    if (objTransaction instanceof Transaction(String sender, String receiver, int amount)) {
+
+      System.out.println(sender + " " + receiver + " " + amount);
+    }
+  }
+
   public static void main(String[] args) {
     // instanceofOld();
-    instanceofNew();
+    // instanceofNew();
+    printTransactionNew(new Transaction("You", "Me", 1_000_000));
   }
 
   private static void instanceofNew() {
