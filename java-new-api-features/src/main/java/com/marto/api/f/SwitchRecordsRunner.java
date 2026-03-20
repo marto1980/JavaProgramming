b14bd2b6fc93bd3ea66050256d76c5b8f2fd56a7
Martin Ivanov <marto1980@gmail.com>
2026-03-20 17:53:08 +0200

    switch with record Patters: Destruction

--- java-new-api-features/src/main/java/com/marto/api/f/SwitchRecordsRunner.java
+++ java-new-api-features/src/main/java/com/marto/api/f/SwitchRecordsRunner.java
@@ -2,21 +2,31 @@ package com.marto.api.f;
 
 public class SwitchRecordsRunner {
 
-  interface CustomerMessage {}
+  sealed interface CustomerMessage permits Message, Feedback {}
 
   record Message(String text) implements CustomerMessage {}
 
   record Feedback(int rating, String description) implements CustomerMessage {}
 
+  record SupportRequest(String user, CustomerMessage message) {}
+
   public static void main(String[] args) {
-    // CustomerMessage customerMessage = new Message("Hello");
-    CustomerMessage customerMessage = new Feedback(5, "Really good!");
+    CustomerMessage customerMessage = new Message("Hello");
+    // CustomerMessage customerMessage = new Feedback(5, "Really good!");
     String response =
         switch (customerMessage) {
-          case Message message -> message.text();
-          case Feedback feedback -> feedback.description();
-          default -> "Default value";
+          case Message(String text) -> text;
+          case Feedback(int rating, String description) -> description;
         };
     System.out.println(response);
+
+    SupportRequest request = new SupportRequest("Rahul", new Feedback(5, "Really good!"));
+
+    String responseMessage =
+        switch (request) {
+          case SupportRequest(String user, Message(String text)) -> text;
+          case SupportRequest(String user, Feedback(int rating, String description)) -> description;
+        };
+    System.out.println(responseMessage);
   }
 }
