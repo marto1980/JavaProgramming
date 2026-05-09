d03cce6420e67881d0a6bb5e0ea0a558352b2d05
Martin Ivanov <marto1980@gmail.com>
2026-05-09 11:49:49 +0300

    Understanding Loose Coupling and Tight Coupling

--- learn-spring-framework/src/main/java/com/marto/learn_spring_framework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/marto/learn_spring_framework/AppGamingBasicJava.java
@@ -1,13 +1,14 @@
 package com.marto.learn_spring_framework;
 
 import com.marto.learn_spring_framework.game.GameRunner;
-import com.marto.learn_spring_framework.game.MarioGame;
+import com.marto.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
   public static void main(String[] args) {
-    var marioGame = new MarioGame();
-    var gameRunner = new GameRunner(marioGame);
+    // var marioGame = new MarioGame();
+    var superContraGame = new SuperContraGame();
+    var gameRunner = new GameRunner(superContraGame);
     gameRunner.run();
   }
 }
--- learn-spring-framework/src/main/java/com/marto/learn_spring_framework/game/GameRunner.java
+++ learn-spring-framework/src/main/java/com/marto/learn_spring_framework/game/GameRunner.java
@@ -1,9 +1,9 @@
 package com.marto.learn_spring_framework.game;
 
 public class GameRunner {
-  private MarioGame game;
+  private SuperContraGame game;
 
-  public GameRunner(MarioGame game) {
+  public GameRunner(SuperContraGame game) {
     this.game = game;
   }
 
