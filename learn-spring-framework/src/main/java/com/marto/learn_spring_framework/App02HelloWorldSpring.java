4319547d64149e421c96e3c83de1a9b886850926
Martin Ivanov <marto1980@gmail.com>
2026-05-09 16:00:02 +0300

    Create More Java Spring Beans in Spring Java Configuration File

--- learn-spring-framework/src/main/java/com/marto/learn_spring_framework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/marto/learn_spring_framework/App02HelloWorldSpring.java
@@ -13,5 +13,8 @@ public class App02HelloWorldSpring {
 
     // Retrieves Beans managed by Spring
     System.out.println(context.getBean("name"));
+    System.out.println(context.getBean("age"));
+    System.out.println(context.getBean("person"));
+    System.out.println(context.getBean("address"));
   }
 }
