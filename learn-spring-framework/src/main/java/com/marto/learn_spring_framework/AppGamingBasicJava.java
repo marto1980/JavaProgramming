5ec20a6f616697ff3fc0e4951cd5e78857a62f6c
Martin Ivanov <marto1980@gmail.com>
2026-05-09 12:02:30 +0300

    Introduce Java Interface to Make App Loosely Coupled

--- learn-spring-framework/src/main/java/com/marto/learn_spring_framework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/marto/learn_spring_framework/AppGamingBasicJava.java
@@ -1,14 +1,14 @@
 package com.marto.learn_spring_framework;
 
 import com.marto.learn_spring_framework.game.GameRunner;
-import com.marto.learn_spring_framework.game.SuperContraGame;
+import com.marto.learn_spring_framework.game.MarioGame;
 
 public class AppGamingBasicJava {
 
   public static void main(String[] args) {
-    // var marioGame = new MarioGame();
-    var superContraGame = new SuperContraGame();
-    var gameRunner = new GameRunner(superContraGame);
+    var game = new MarioGame();
+    // var game = new SuperContraGame();
+    var gameRunner = new GameRunner(game);
     gameRunner.run();
   }
 }
