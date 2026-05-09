ee4979a06edb79eed7377ad9c1cf4841154ed0dc
Martin Ivanov <marto1980@gmail.com>
2026-05-09 11:30:01 +0300

    Add Buttons to Our Spring Gaming App

--- learn-spring-framework/src/main/java/com/marto/learn_spring_framework/game/MarioGame.java
+++ learn-spring-framework/src/main/java/com/marto/learn_spring_framework/game/MarioGame.java
@@ -1,3 +1,19 @@
 package com.marto.learn_spring_framework.game;
 
-public class MarioGame {}
+public class MarioGame {
+  public void up() {
+    System.out.println("Jump");
+  }
+
+  public void down() {
+    System.out.println("Go into a hole");
+  }
+
+  public void left() {
+    System.out.println("Go Back");
+  }
+
+  public void right() {
+    System.out.println("Accelerate");
+  }
+}
