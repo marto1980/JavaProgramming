e12ce1ca1ecbb1ee28f608c376a42be162fdda0f
Martin Ivanov <marto1980@gmail.com>
2026-05-09 12:15:50 +0300

    Add a PacMan Game

--- learn-spring-framework/src/main/java/com/marto/learn_spring_framework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/marto/learn_spring_framework/AppGamingBasicJava.java
@@ -1,13 +1,14 @@
 package com.marto.learn_spring_framework;
 
 import com.marto.learn_spring_framework.game.GameRunner;
-import com.marto.learn_spring_framework.game.MarioGame;
+import com.marto.learn_spring_framework.game.PacManGame;
 
 public class AppGamingBasicJava {
 
   public static void main(String[] args) {
-    var game = new MarioGame();
+    // var game = new MarioGame();
     // var game = new SuperContraGame();
+    var game = new PacManGame();
     var gameRunner = new GameRunner(game);
     gameRunner.run();
   }
