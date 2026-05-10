93bfd18e521a03750045686db4e74f62748f61d1
Martin Ivanov <marto1980@gmail.com>
2026-05-10 11:25:20 +0300

    Explore Spring Framework Bean Auto Wiring

--- learn-spring-framework/src/main/java/com/marto/learn_spring_framework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/marto/learn_spring_framework/App02HelloWorldSpring.java
@@ -18,7 +18,8 @@ public class App02HelloWorldSpring {
     System.out.println(context.getBean("person2MethodCall"));
     System.out.println(context.getBean("address2"));
     System.out.println(context.getBean("person3Parameters"));
-    // System.out.println(context.getBean(Address.class));
-    context.close();
+    System.out.println(context.getBean(Person.class));
+    // Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+    // context.close();
   }
 }
