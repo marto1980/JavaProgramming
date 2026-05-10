111e0fed41ed5a37315374efa678012556ae0c76
Martin Ivanov <marto1980@gmail.com>
2026-05-10 14:16:05 +0300

    Get Spring Framework to Create and Manage Your Java Objects

--- learn-spring-framework_02/src/main/java/com/marto/learn_spring_framework/App03GamingSpringBeans.java
+++ learn-spring-framework_02/src/main/java/com/marto/learn_spring_framework/App03GamingSpringBeans.java
@@ -2,13 +2,29 @@ package com.marto.learn_spring_framework;
 
 import com.marto.learn_spring_framework.game.GameRunner;
 import com.marto.learn_spring_framework.game.GamingConsole;
+import com.marto.learn_spring_framework.game.PacManGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class App03GamingSpringBeans {
+
+  @Bean
+  public GamingConsole game() {
+    var game = new PacManGame();
+    return game;
+  }
+
+  @Bean
+  public GameRunner gameRunner(GamingConsole game) {
+    var gameRunner = new GameRunner(game);
+    return gameRunner;
+  }
 
   public static void main(String[] args) {
 
-    try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+    try (var context = new AnnotationConfigApplicationContext(App03GamingSpringBeans.class)) {
       context.getBean(GamingConsole.class).up();
 
       context.getBean(GameRunner.class).run();
