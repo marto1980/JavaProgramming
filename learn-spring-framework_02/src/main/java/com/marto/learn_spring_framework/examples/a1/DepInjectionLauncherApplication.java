4a88991043eb30a422d03093d266635f694d5076
Martin Ivanov <marto1980@gmail.com>
2026-05-10 17:21:14 +0300

    Get Started with Field Injection

--- learn-spring-framework_02/src/main/java/com/marto/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework_02/src/main/java/com/marto/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
@@ -1,9 +1,27 @@
 package com.marto.learn_spring_framework.examples.a1;
 
 import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+class YourBusinessClass {
+  @Autowired private Dependency1 dependency1;
+  @Autowired private Dependency2 dependency2;
+
+  public String toString() {
+    return "Using " + dependency1 + " and " + dependency2;
+  }
+}
+
+@Component
+class Dependency1 {}
+
+@Component
+class Dependency2 {}
 
 @Configuration
 @ComponentScan
@@ -15,6 +33,7 @@ public class DepInjectionLauncherApplication {
         new AnnotationConfigApplicationContext(DepInjectionLauncherApplication.class)) {
 
       Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+      System.out.println(context.getBean(YourBusinessClass.class));
     }
   }
 }
