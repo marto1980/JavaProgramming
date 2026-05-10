dbdbed22cfb036af8a3f4306d28616f05b22a65c
Martin Ivanov <marto1980@gmail.com>
2026-05-10 10:18:18 +0300

    Reuse Existing Spring Beans for Auto Wiring

--- learn-spring-framework/src/main/java/com/marto/learn_spring_framework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/marto/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.marto.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) {}
+record Person(String name, int age, Address address) {}
 
 record Address(String firstLine, String city) {}
 
@@ -21,11 +21,26 @@ public class HelloWorldConfiguration {
 
   @Bean
   public Person person() {
-    return new Person("Ravi", 20);
+    return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));
+  }
+
+  @Bean
+  public Person person2MethodCall() {
+    return new Person(name(), age(), address());
+  }
+
+  @Bean
+  public Person person3Parameters(String name, int age, Address address3) { // name, age, address2
+    return new Person(name, age, address3);
   }
 
   @Bean(name = "address2")
   public Address address() {
     return new Address("Baker Street", "London");
+  }
+
+  @Bean(name = "address3")
+  public Address address3() {
+    return new Address("Motinagar", "Hyderabad");
   }
 }
