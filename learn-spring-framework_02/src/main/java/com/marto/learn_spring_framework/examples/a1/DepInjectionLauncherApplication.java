25d0aa5398e843764f2824d92393e8512d2ef3c6
Martin Ivanov <marto1980@gmail.com>
2026-05-10 18:17:28 +0300

    Field-Based Dependency Injection

--- learn-spring-framework_02/src/main/java/com/marto/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework_02/src/main/java/com/marto/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
@@ -9,8 +9,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 class YourBusinessClass {
-  @Autowired private Dependency1 dependency1;
-  @Autowired private Dependency2 dependency2;
+  private Dependency1 dependency1;
+
+  @Autowired
+  public void setDependency1(Dependency1 dependency1) {
+    System.out.println("Setter Injection: setDependency1");
+    this.dependency1 = dependency1;
+  }
+
+  @Autowired
+  public void setDependency2(Dependency2 dependency2) {
+    System.out.println("Setter Injection: setDependency2");
+    this.dependency2 = dependency2;
+  }
+
+  private Dependency2 dependency2;
 
   public String toString() {
     return "Using " + dependency1 + " and " + dependency2;
