c9024c88e2c75d589ba0ca0dd79cb72b10abec0f
Martin Ivanov <marto1980@gmail.com>
2026-05-10 12:03:45 +0300

    Explore Spring Framework Primary and Qualifier Annotations

--- learn-spring-framework/src/main/java/com/marto/learn_spring_framework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/marto/learn_spring_framework/App02HelloWorldSpring.java
@@ -19,6 +19,8 @@ public class App02HelloWorldSpring {
     System.out.println(context.getBean("address2"));
     System.out.println(context.getBean("person3Parameters"));
     System.out.println(context.getBean(Person.class));
+    System.out.println(context.getBean(Address.class));
+    System.out.println(context.getBean("person5Qualifier"));
     // Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     // context.close();
   }
--- learn-spring-framework/src/main/java/com/marto/learn_spring_framework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/marto/learn_spring_framework/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.marto.learn_spring_framework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person(String name, int age, Address address) {}
 
@@ -34,12 +36,28 @@ public class HelloWorldConfiguration {
     return new Person(name, age, address3);
   }
 
+  @Bean
+  @Primary
+  public Person person4Parameters(String name, int age, Address address) { // name, age, address2
+    return new Person(name, age, address);
+  }
+
+  @Bean
+  public Person person5Qualifier(
+      String name,
+      int age,
+      @Qualifier("address3Qualifier") Address address) { // name, age, address2
+    return new Person(name, age, address);
+  }
+
   @Bean(name = "address2")
+  @Primary
   public Address address() {
     return new Address("Baker Street", "London");
   }
 
   @Bean(name = "address3")
+  @Qualifier("address3Qualifier")
   public Address address3() {
     return new Address("Motinagar", "Hyderabad");
   }
