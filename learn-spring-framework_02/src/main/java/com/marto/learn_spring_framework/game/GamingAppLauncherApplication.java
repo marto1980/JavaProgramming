08cc544ee7c7a71c56e90f3bbce65adb2db40b0b
Martin Ivanov <marto1980@gmail.com>
2026-05-10 16:55:45 +0300

    01: Exploring Spring Framework - Different Types of Dependency

--- learn-spring-framework_02/src/main/java/com/marto/learn_spring_framework/game/GamingAppLauncherApplication.java
+++ learn-spring-framework_02/src/main/java/com/marto/learn_spring_framework/game/GamingAppLauncherApplication.java
@@ -1,7 +1,5 @@
-package com.marto.learn_spring_framework;
+package com.marto.learn_spring_framework.game;
 
-import com.marto.learn_spring_framework.game.GameRunner;
-import com.marto.learn_spring_framework.game.GamingConsole;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
