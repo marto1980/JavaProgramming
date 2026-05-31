5eb83e6edc1bdfcb45ee18d343a87dffb02a2c32
Martin Ivanov <marto1980@gmail.com>
2026-05-31 09:52:54 +0300

    Get Production Ready with Spring Boot: Configuration Properties

--- learn-spring-boot/src/main/java/com/marto/springboot/learn_spring_boot/CourseController.java
+++ learn-spring-boot/src/main/java/com/marto/springboot/learn_spring_boot/CourseController.java
@@ -10,6 +10,9 @@ public class CourseController {
   @RequestMapping("/courses")
   public List<Course> retrieveAllCourses() {
     return Arrays.asList(
-        new Course(1, "Learn AWS", "in28minutes"), new Course(2, "Learn DevOps", "in28minutes"));
+        new Course(1, "Learn AWS", "in28minutes"),
+        new Course(2, "Learn DevOps", "in28minutes"),
+        new Course(3, "Learn Azure", "in28minutes"),
+        new Course(4, "Learn GCP", "in28minutes"));
   }
 }
