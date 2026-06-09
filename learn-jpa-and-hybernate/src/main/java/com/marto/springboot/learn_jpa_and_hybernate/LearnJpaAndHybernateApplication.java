8e413a752a218b2ea3ee6b79fb8321c0fa3dfbdc
Martin Ivanov <marto1980@gmail.com>
2026-06-09 17:55:04 +0300

    Launch up H2 Console and Create Course Table in H2

---
+++ learn-jpa-and-hybernate/src/main/java/com/marto/springboot/learn_jpa_and_hybernate/LearnJpaAndHybernateApplication.java
@@ -0,0 +1,13 @@
+package com.marto.springboot.learn_jpa_and_hybernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaAndHybernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaAndHybernateApplication.class, args);
+	}
+
+}
---
+++ learn-jpa-and-hybernate/src/test/java/com/marto/springboot/learn_jpa_and_hybernate/LearnJpaAndHybernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marto.springboot.learn_jpa_and_hybernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHybernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
