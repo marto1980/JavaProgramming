f970ec83b0680ca75cd4ca94cfa91ffad771f616
Martin Ivanov <marto1980@gmail.com>
2026-06-10 10:43:35 +0300

    Insert Hardcoded Data Using Spring JDBC

---
+++ learn-jpa-and-hybernate/src/main/java/com/marto/springboot/learn_jpa_and_hybernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,21 @@
+package com.marto.springboot.learn_jpa_and_hybernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+
+  @Autowired private JdbcTemplate springJdbcTemplate;
+
+  private static String INSERT_QUERY =
+      """
+      insert into course (id, name, author)
+      values(1, 'Learn AWS', 'in28minutes');
+      """;
+
+  public void insert() {
+    springJdbcTemplate.update(INSERT_QUERY);
+  }
+}
