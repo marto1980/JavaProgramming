e0e76c6c77b4824e542ad468df73a8fbc333a795
Martin Ivanov <marto1980@gmail.com>
2026-06-11 14:24:25 +0300

    Build REST API to Update a Course

--- restapi/src/main/java/com/marto/restapi/controller/CourseController.java
+++ restapi/src/main/java/com/marto/restapi/controller/CourseController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +35,13 @@ public class CourseController {
   // POST - Create a new resource (/courses)
   @PostMapping("/courses")
   public void createCourse(@RequestBody Course course) {
+    repository.save(course);
+  }
+
+  // PUT - Update/Replace a resource (/courses/1)
+  @PutMapping("/courses/{id}")
+  public void updateCourse(@PathVariable Long id, @RequestBody Course course) {
+    course.setId(id);
     repository.save(course);
   }
 }
